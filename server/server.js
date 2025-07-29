ccf534ab89a3f2ea5f4b6dc9b57f1795b10c5c8a
Jaquavia Walker <jaquavia.walker@copeland.com>
2025-07-29 17:16:39 -0400

    server set up

---
+++ server/server.js
@@ -0,0 +1,13 @@
+    const express = require('express');
+    const app = express();
+    const PORT = process.env.PORT || 3000; 
+
+    // Define a basic route
+    app.get('/', (req, res) => {
+        res.send('Hello from Express!');
+    });
+
+    // Start the server
+    app.listen(PORT, () => {
+        console.log(`Express server running at http://localhost:${PORT}/`);
+    });
